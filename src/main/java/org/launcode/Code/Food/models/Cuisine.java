cb81b27f63dbd0c94acaed027104b1a7503b94ab
kaviya <kaviya0925@gmail.com>
2022-01-13 18:39:45 -0600

    removed cuisine description

--- src/main/java/org/launcode/Code/Food/models/Cuisine.java
+++ src/main/java/org/launcode/Code/Food/models/Cuisine.java
@@ -8,25 +8,16 @@ import javax.validation.constraints.Size;
 @Entity
 public class Cuisine extends AbstractEntity{
 
-    @NotBlank
-    @Size(min=3,max=300,message ="location must be between 3 to 300 characters")
-    private String description;
+
 
     public Cuisine() {
     }
 
-    public String getDescription() {
-        return description;
-    }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
 
     @Override
     public String toString() {
         return "Cuisine{" +
-                "description='" + description + '\'' +
                 "} " + super.toString();
     }
 }
