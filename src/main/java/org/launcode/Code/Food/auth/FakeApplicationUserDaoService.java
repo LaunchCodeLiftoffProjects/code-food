505746c73ff1185ba3dbc3e3f9cf9e7166c2b04b
Steve <stevethats@yahoo.com>
2022-01-25 20:10:00 -0600

    Rewrote the users to get ready to be implemented into a database

--- src/main/java/org/launcode/Code/Food/auth/FakeApplicationUserDaoService.java
+++ src/main/java/org/launcode/Code/Food/auth/FakeApplicationUserDaoService.java
@@ -1,39 +1,65 @@
-//package org.launcode.Code.Food.auth;
-//
-//import com.google.common.collect.Lists;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import static org.launcode.Code.Food.security.ApplicationUserRole.USER;
-//
-//public class FakeApplicationUserDaoService implements ApplicationUserDAO{
-//
-//    private final PasswordEncoder passwordEncoder;
-//
-//    @Autowired
-//    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
-//        this.passwordEncoder = passwordEncoder;
-//    }
-//
-//    @Override
-//    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
-//        return Optional.empty();
-//    }
-//
-//    private List<ApplicationUser> getApplicationUsers() {
-//        List<ApplicationUser> applicationUsers = Lists.newArrayList(
-//            new ApplicationUser(
-//                    "annasmith",
-//                    passwordEncoder.encode("password"),
-//                    USER.getGrantedAuthorities(),
-//
-//            )
-//        );
-//
-//        return applicationUsers;
-//    }
-//
-//}
+package org.launcode.Code.Food.auth;
+
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.launcode.Code.Food.security.ApplicationUserRole.*;
+
+@Repository("fake")
+public class FakeApplicationUserDaoService implements ApplicationUserDAO{
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public FakeApplicationUserDaoService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username) {
+        return getApplicationUsers()
+                .stream()
+                .filter(applicationUser -> username.equals(applicationUser.getUsername()))
+                .findFirst();
+    }
+
+    private List<ApplicationUser> getApplicationUsers() {
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+            new ApplicationUser(
+                    passwordEncoder.encode("password"),
+                    "annasmith",
+                    USER.getGrantedAuthorities(),
+                    true,
+                    true,
+                    true,
+                    true
+            ),
+                new ApplicationUser(
+                        passwordEncoder.encode("password123"),
+                        "linda",
+                        ADMIN.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        passwordEncoder.encode("password123"),
+                        "tom",
+                        ADMINTRAINEE.getGrantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+
+        return applicationUsers;
+    }
+
+}
--- src/main/java/org/launcode/Code/Food/security/ApplicationSecurityConfig.java
+++ src/main/java/org/launcode/Code/Food/security/ApplicationSecurityConfig.java
@@ -1,18 +1,16 @@
 package org.launcode.Code.Food.security;
 
+import org.launcode.Code.Food.auth.ApplicationUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import java.util.concurrent.TimeUnit;
 
@@ -24,10 +22,12 @@ import static org.launcode.Code.Food.security.ApplicationUserRole.*;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
     @Autowired
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -68,34 +68,47 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-        UserDetails annaSmithUser = User.builder()
-                .username("annasmith")
-                .password(passwordEncoder.encode("password"))
-                .authorities(USER.getGrantedAuthorities())
-                .build();
-        //Manually adds Anna Smith as a USER
-
-        UserDetails lindaUser = User.builder()
-                .username("linda")
-                .password(passwordEncoder.encode("password123"))
-                .authorities(ADMIN.getGrantedAuthorities())
-                .build();
-        //Manually adds Linda as an ADMIN
-
-        UserDetails tomUser = User.builder()
-                .username("tom")
-                .password(passwordEncoder.encode("password123"))
-                .authorities(ADMINTRAINEE.getGrantedAuthorities())
-                .build();
-        //Manually adds Tom as an ADMIN TRAINEE
-
-        return new InMemoryUserDetailsManager(
-                annaSmithUser,
-                lindaUser,
-                tomUser
-        );
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+    }
+
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails annaSmithUser = User.builder()
+//                .username("annasmith")
+//                .password(passwordEncoder.encode("password"))
+//                .authorities(USER.getGrantedAuthorities())
+//                .build();
+//        //Manually adds Anna Smith as a USER
+//
+//        UserDetails lindaUser = User.builder()
+//                .username("linda")
+//                .password(passwordEncoder.encode("password123"))
+//                .authorities(ADMIN.getGrantedAuthorities())
+//                .build();
+//        //Manually adds Linda as an ADMIN
+//
+//        UserDetails tomUser = User.builder()
+//                .username("tom")
+//                .password(passwordEncoder.encode("password123"))
+//                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+//                .build();
+//        //Manually adds Tom as an ADMIN TRAINEE
+//
+//        return new InMemoryUserDetailsManager(
+//                annaSmithUser,
+//                lindaUser,
+//                tomUser
+//        );
+//    }
 
 }
